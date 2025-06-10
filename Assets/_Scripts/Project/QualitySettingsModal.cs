7d74bf45ae05eda73019291e8ece2ea0d0fb8f14
Feverfew826 <lightkill826@gmail.com>
2025-06-11 04:06:47 +0900

    UI를 폴리싱하고, QualitySetting UI 를 동작하게 함.

--- Assets/_Scripts/Project/QualitySettingsModal.cs
+++ Assets/_Scripts/Project/QualitySettingsModal.cs
@@ -2,6 +2,8 @@ using System.Threading;
 
 using Cysharp.Threading.Tasks;
 
+using UniRx;
+
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -10,22 +12,35 @@ using YachuDice.AddressableWrapper;
 
 public class QualitySettingsModal : MonoBehaviour
 {
-    [SerializeField] private Button _saveButton;
+    [SerializeField] private Button _veryLowButton;
+    [SerializeField] private Button _lowButton;
+    [SerializeField] private Button _mediumButton;
+    [SerializeField] private Button _highButton;
+    [SerializeField] private Button _veryHighButton;
+    [SerializeField] private Button _ultraButton;
+    [SerializeField] private Button _exitButton;
 
     public static async UniTask OpenQualitySettingsModalAsync(CancellationToken cancellationToken)
     {
         var selectionBackedup = EventSystem.current.currentSelectedGameObject;
 
-        using var qualitySettingsLoads = await AddressableWrapper.DisposableInstantiateAsync<QualitySettingsModal>("QualitySettings.prefab", cancellationToken: cancellationToken);
-        var qualitySettings = qualitySettingsLoads.Instance;
-        EventSystem.current.SetSelectedGameObject(qualitySettings.gameObject);
+        using var qualitySettingsModalLoads = await AddressableWrapper.DisposableInstantiateAsync<QualitySettingsModal>("QualitySettings.prefab", cancellationToken: cancellationToken);
+        var qualitySettingsModal = qualitySettingsModalLoads.Instance;
+        EventSystem.current.SetSelectedGameObject(qualitySettingsModal.gameObject);
 
-        await qualitySettings.WaitAsync(cancellationToken);
+        qualitySettingsModal._veryLowButton.OnClickAsObservable().Subscribe(_ => QualitySettings.SetQualityLevel(0)).AddTo(qualitySettingsModal);
+        qualitySettingsModal._lowButton.OnClickAsObservable().Subscribe(_ => QualitySettings.SetQualityLevel(1)).AddTo(qualitySettingsModal);
+        qualitySettingsModal._mediumButton.OnClickAsObservable().Subscribe(_ => QualitySettings.SetQualityLevel(2)).AddTo(qualitySettingsModal);
+        qualitySettingsModal._highButton.OnClickAsObservable().Subscribe(_ => QualitySettings.SetQualityLevel(3)).AddTo(qualitySettingsModal);
+        qualitySettingsModal._veryHighButton.OnClickAsObservable().Subscribe(_ => QualitySettings.SetQualityLevel(4)).AddTo(qualitySettingsModal);
+        qualitySettingsModal._ultraButton.OnClickAsObservable().Subscribe(_ => QualitySettings.SetQualityLevel(5)).AddTo(qualitySettingsModal);
+
+        await qualitySettingsModal.WaitAsync(cancellationToken);
         EventSystem.current.SetSelectedGameObject(selectionBackedup);
     }
 
     public async UniTask WaitAsync(CancellationToken cancellationToken)
     {
-        await _saveButton.OnClickAsync();
+        await _exitButton.OnClickAsync();
     }
 }
