cd3a47e029da3dc683ba6b466f0e8e218cabae48
Jeong Hwansu <lightkill826@gmail.com>
2025-06-10 20:07:54 +0900

    IngameDebugConsoleStarter에서 컴파일러 경고가 표시되지 않도록 함.

--- Assets/_Scripts/Utilities/IngameDebugConsoleStarter.cs
+++ Assets/_Scripts/Utilities/IngameDebugConsoleStarter.cs
@@ -17,7 +17,9 @@ namespace YachuDice.Utilities
             if (YachuDice.Environment.Development.Development.IsDevelopment == false)
                 return;
 
+#pragma warning disable CS0162 // 접근할 수 없는 코드가 있습니다.
             _ingameDebugConsoleLoads = await AddressableWrapper.AddressableWrapper.DisposableInstantiateAsync<Transform>("IngameDebugConsole.prefab", cancellationToken: cancellationToken);
+#pragma warning restore CS0162 // 접근할 수 없는 코드가 있습니다.
         }
     }
 }
